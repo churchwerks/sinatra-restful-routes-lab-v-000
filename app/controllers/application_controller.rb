2dd661002e251febe1998b9c13ad4db3d3652005
churchwerks <steven.lennis@icloud.com>
2018-01-31 19:25:01 -0600

    Broke something

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -5,11 +6,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+#Index Action
   get '/' do
+    binding.pry
+    @recipes = Recipes.all
     erb :index
   end
+#Create Action
+  get '/new' do
 
-  get '/recipes' do
+    erb :new
+  end
+
+  post '/recipes/new'do
+    @recipe = Recipes.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time] )
+    redirect to "/#{@recipe.id}"
+  end
+#Show Action
+  get '/:id' do
     erb :recipes
   end
+
 end
